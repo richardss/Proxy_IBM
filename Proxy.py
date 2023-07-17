5bffea3d01979e8a34051dd720fba03d72881b20
Riccardo Resta <rolandka88@gmail.com>
2023-07-17 11:27:45 +0200

    Update Proxy.py

--- Proxy.py
+++ Proxy.py
@@ -1,28 +1,29 @@
 from twisted.internet import reactor, protocol, threads
 from twisted.web.client import Agent
 from twisted.web.http_headers import Headers
-import threading
 
 class ProxyProtocol(protocol.Protocol):
     def __init__(self, agent):
         self.agent = agent
+        self.buffer = []
 
     def connectionMade(self):
         self.buffer = []
 
     def dataReceived(self, data):
         self.buffer.append(data)
-        self.forwardData(data)
+        self.forwardData()
 
-    def forwardData(self, data):
+    def forwardData(self):
+        content = b''.join(self.buffer)
         d = threads.deferToThread(
-            agent.request,
+            self.agent.request,
             b"POST",
             b"WEBHOOK_URL",
             Headers({"Content-Type": ["application/json"]}),
-            b''.join(self.buffer)
+            content
         )
-        d.addCallback(lambda _: self.transport.write(data))
+        d.addCallback(lambda _: self.transport.write(content))
 
 class ProxyFactory(protocol.Factory):
     def __init__(self, agent):
@@ -31,15 +32,14 @@ class ProxyFactory(protocol.Factory):
     def buildProtocol(self, addr):
         return ProxyProtocol(self.agent)
 
-# Crea l'oggetto Agent per effettuare le richieste HTTP
+# Create an instance of the Agent to make HTTP requests
 agent = Agent(reactor)
 
-# Crea l'istanza del factory del proxy
+# Create the proxy factory instance
 proxyFactory = ProxyFactory(agent)
 
-# Avvia il proxy su localhost, porta 5529
+# Start the proxy on localhost, port 5529
 reactor.listenTCP(5529, proxyFactory)
 
-# Avvia il reactor di Twisted in un thread separato
-reactor_thread = threading.Thread(target=reactor.run, args=(False,))
-reactor_thread.start()
+# Start the Twisted reactor
+reactor.run()
